e027ffda11e4b3a5b6bf2b09935e6ed08eb8b29a
jaakaappi <diapaasi@gmail.com>
2014-03-06 18:47:51 +0200

    Starting connection making

---
+++ OpenCiv/Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Handles nearly everything, core of everything
+/// </summary>
+
+public class Manager : Photon.MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine("Connect");
+    }
+
+    IEnumerator Connect()
+    {
+        while (true)
+        {
+
+        }
+    }
+
+    void OnFailedToConnectToPhoton(DisconnectCause cause)
+    {
+
+    }
+}
